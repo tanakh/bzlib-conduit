5429857ca5decfe7842bb23846f78170dae1679a
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-06-29 14:33:45 +0900

    switch to conduit-0.5 and fix test

--- test/test.hs
+++ test/test.hs
@@ -19,17 +19,23 @@ import Test.QuickCheck.Property
 import Prelude as P
 
 main :: IO ()
-main = hspecX $ do
+main = hspec $ do
   describe "decompress" $ do
     forM_ ["sample1", "sample2", "sample3"] $ \file -> do
       it ("correctly " ++ file ++ ".bz2") $ do
-        dec <- runResourceT $ sourceFile ("test/" ++ file ++ ".bz2") =$= bunzip2 $$ B.take (10^9)
+        dec <- runResourceT $ do
+          sourceFile ("test/" ++ file ++ ".bz2") =$= bunzip2 $$ B.take (10^9)
         ref <- L.readFile ("test/" ++ file ++ ".ref")
         assert $ dec == ref
 
   describe "compress" $ do
-    prop ". decompress == id" $ \((`mod` (2^16)) . abs -> n) -> morallyDubiousIOProperty $ do
-      let bsize = 8192
-      ss <- P.takeWhile (not. null) . P.map (P.take bsize) . iterate (P.drop bsize) <$> replicateM (abs n) randomIO
-      dest <- runResourceT $ C.sourceList (P.map S.pack ss) =$= bzip2 =$= bunzip2 $$ B.take (10^9)
-      return $ dest == L.pack (concat ss)
+    prop ". decompress == id" $ \((`mod` (2^16)) . abs -> n) -> do
+      morallyDubiousIOProperty $ do
+        let bsize = 8192
+        ss <- P.takeWhile (not. null)
+              . P.map (P.take bsize)
+              . P.iterate (P.drop bsize)
+              <$> replicateM (abs n) randomIO
+        dest <- runResourceT $ do
+          C.sourceList (P.map S.pack ss) =$= bzip2 =$= bunzip2 $$ B.take (10^9)
+        return $ dest == L.pack (concat ss)
